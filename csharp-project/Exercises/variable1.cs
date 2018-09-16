2768f270ae7eee0eefb40f94cff6d66088cc448c
yannick <yantra2a@sti-2d.com>
2018-09-16 11:33:51 +0200

    Update variable1.cs

--- csharp-project/Exercises/variable1.cs
+++ csharp-project/Exercises/variable1.cs
@@ -7,16 +7,19 @@ using System.Linq;
 namespace Answer
 {
     
-    public class Hello
-        {
-        //Créer ci-desous la variable demandée
-        
-        
-        
-        }
-
-// { autofold
-    
-}
+   namespace Answer
+{
+	public class UniverseStub
+	{
 // }
-
+public static int CountAllStars(params int[] galaxies){
+	int totalStars = 0;
+	foreach(int stars in galaxies) {
+		totalStars = stars; // fix me!
+	} 
+	return totalStars;
+}
+//{ autofold
+	}
+}
+//}
